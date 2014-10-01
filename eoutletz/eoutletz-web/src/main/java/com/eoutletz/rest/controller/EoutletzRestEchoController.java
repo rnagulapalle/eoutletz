df3161585abee77f5a91fcea208497145e09d9fd
Raj Nagulapalle <rnagulapalle@paypal.com>
2014-09-30 17:13:05 -0700

    added error handling for REST

--- eoutletz/eoutletz-web/src/main/java/com/eoutletz/rest/controller/EoutletzRestEchoController.java
+++ eoutletz/eoutletz-web/src/main/java/com/eoutletz/rest/controller/EoutletzRestEchoController.java
@@ -20,12 +20,17 @@ public class EoutletzRestEchoController extends EoutletzRestBaseController {
 	private EchoService echoService;
 	
 	@RequestMapping(value = "/echo/", method = RequestMethod.GET, produces="application/json")
-	public ResponseEntity<BaseResponseResource> updateUser(HttpServletRequest request) {
+	public ResponseEntity<BaseResponseResource> getEcho(HttpServletRequest request) {
 		
 		BaseResponseResource response = new BaseResponseResource(HttpStatus.OK.value(), echoService.echo());
 		org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
 		headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
 
 		return new ResponseEntity<>(response, headers, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/error/", method = RequestMethod.GET, produces="application/json")
+	public ResponseEntity<BaseResponseResource> getError(HttpServletRequest request) {
+		throw new IllegalArgumentException("Invalid argument found, good luck!");
 	}
 }
